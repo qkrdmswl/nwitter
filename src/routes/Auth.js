3b19c65b551c40dc906b104ea810e97213caa728
qkrdmswl <eunji303@naver.com>
2021-07-02 00:37:56 +0900

    authService incomplete

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,4 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
+import { authService } from "fbase";
 
-const Auth= () => <span>Auth</span>;
+const Auth= () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [newAccount, setNewAccount] = useState("false");
+    const onChange = (event) =>{
+        const {target: {name, value}} = event;
+        if(name === "email"){
+            setEmail(value);
+        } else if(name === "password") {
+            setPassword(value);
+        }
+    };
+    const onSubmit = async(event) => {
+        event.preventDefault();
+        try{
+            if(newAccount){
+                //create account
+               const data = await authService.createUserWithEmailAndPassword(
+                   email,
+                   password
+                );
+            } else {
+                //login
+                const data = await authService.signInWithEmailAndPassword(
+                    email,
+                    password
+                );
+            }
+        } catch(error) {
+            console.log(error);
+        }
+    };
+    return (
+        <div>
+            <form onSubmit={onSubmit}>
+                <input 
+                    name="email"
+                    type="email"
+                    placeholder="Email"
+                    required
+                    value={email} 
+                    onChange={onChange}
+                />
+                <input
+                    name="password"
+                    type="password"
+                    placeholder="Password"
+                    required
+                    value={password}
+                    onChange={onChange}
+                />
+                <input type="submit" value={newAccount ? "Create Account" : "Log In"} />
+            </form>
+            <div>
+                <button>Continue with Google</button>
+            </div>
+        </div>
+    );
+};
 export default Auth;
