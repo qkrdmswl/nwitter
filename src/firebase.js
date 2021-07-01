c064708240edef7c6bbc204d7c086bc3c32e1229
qkrdmswl <eunji303@naver.com>
2021-07-01 22:27:31 +0900

    connect firebase

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDEC0ExnPaPdHVmp7_yNM5pN28qKUvAxQ4",
+    authDomain: "nwitter-4660c.firebaseapp.com",
+    projectId: "nwitter-4660c",
+    storageBucket: "nwitter-4660c.appspot.com",
+    messagingSenderId: "805397589735",
+    appId: "1:805397589735:web:a646a6a775fc9e4c4e3c42"
+  };
+
+  export default firebase.initializeApp(firebaseConfig);
+
